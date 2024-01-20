4105c56bf267ea3c23a71a17c92f3ed56ede5332
Luciano Martins <devcsharpluciano@gmail.com>
2024-01-20 19:50:23 -0300

    criando model cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProjeto.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }              
+        public string Nome { get; set; }    
+        
+    }
+}
